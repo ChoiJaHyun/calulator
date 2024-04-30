1dd82835f94641ec19313429708809af950327f1
ChoiJaHyun <dngel9880@gmail.com>
2024-04-30 14:05:54 +0900

    20240430

--- main.js
+++ main.js
@@ -3,3 +3,71 @@ let result = 0;
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) {
+    console.log("end");
+
+    return;
+  }
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
+/*
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) {
+    console.log("end");
+
+    return;
+  }
+  
+  
+  
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
+/*
+function onClickNumber(number) {
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+
+  console.log(resultEl);
+}
+
+console.log(resultEl.innerHTML.length);
+/*
+function onClickNumber(number) {
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
+/*
+function onClickNumber(number) {
+  resultEl.innerHTML = number;
+}
+// 0 일때만 추가 -> 교체
+function onClickNumber(number) {
+  resultEl.innerHTML += number;
+}
+
+/*function test() {
+ function onClickNumber(number) {
+  resultEl.innerHTML += number;
+} alert("Test!");
+}*/
+//1만 클릭됨
+/*function onClickNumber() {
+  resultEl.innerHTML = 1;
+} */
